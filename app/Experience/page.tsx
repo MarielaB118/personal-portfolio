8b2f5b465ddcbf4ca6faf7b3234b58e2924377e6
Mariela Badillo <41022186+MarielaB118@users.noreply.github.com>
2024-06-03 16:43:19 -0400

    create experience page and experience card
    Created experience page to highlight work and education. Created an experience card to be reused in experience and project pages.

--- app/Experience/page.tsx
+++ app/Experience/page.tsx
@@ -1,7 +1,28 @@
-
-
+import { raleway } from "@/app/ui/fonts"
+import ExperienceCard from "@/app/ui/Experience/ExperienceCard"
+import Image from "next/image"
 export default function Page() {
     return (
-        <div>Experience</div>
+        <div className="relative flex flex-row h-auto pt-20">
+            <div className='basis-1/2'>
+                <div className={`${raleway.className} text-3xl bg-lightBlue w-full p-4`}>Work Experience</div>
+                <ExperienceCard experienceType='work' />
+            </div>
+            <div className='basis-1/2'>
+                <div className={`${raleway.className} text-3xl bg-lightBlue w-2/3 p-4 rounded-r-full`}>Education</div>
+                <ExperienceCard experienceType='education' />
+                
+            </div>
+            {/* background animated svg */}
+            <div className='absolute bottom-0 right-0'>
+                <Image
+                    src='/pattern.png'
+                    width={500}
+                    height={500}
+                    className='relative -z-10 scale-x-flip' 
+                    alt='svg image with half circle, star, and pointed flower'
+                />
+                </div>
+        </div>
     )
 }
---
+++ app/lib/experience/projectExperience.ts
@@ -0,0 +1,11 @@
+const ProjectExperience = [
+    {
+        title: 'title',
+        company: 'company',
+        duration: 'duration',
+        description: 'description',
+        skills: 'skills'
+    }
+];
+
+export default ProjectExperience; 
