f2ac113784a941ae666c5716c1c63222388b4743
Mariela Badillo <41022186+MarielaB118@users.noreply.github.com>
2024-06-03 13:30:10 -0400

    escape apostrophe bug

--- app/ui/Home/TypingEffect.tsx
+++ app/ui/Home/TypingEffect.tsx
@@ -3,7 +3,7 @@ import { Typewriter, Cursor } from "nextjs-simple-typewriter";
 export default function TypingEffect(){
 
     return(
-        <div>I'm 
+        <div>I&apos;m 
             <Typewriter
                 words={[' a learner.', ' a developer.', ' an innovator.', ' Mariela.']}
             />
