f1992e485887926e5c98a6051cb8a816bd9ebbc2
Mariela Badillo <41022186+MarielaB118@users.noreply.github.com>
2024-06-03 18:35:13 -0400

    create project experience page

--- app/Experience/page.tsx
+++ app/Experience/page.tsx
@@ -2,6 +2,33 @@
 
 export default function Page() {
     return (
-        <div>Experience</div>
+        <div className="relative flex flex-row h-auto pt-20">
+
+            {/* work experience */}
+            <div className='basis-1/2'>
+                <div className={`${raleway.className} text-3xl bg-lightBlue w-full p-4`}>Work Experience</div>
+                <ExperienceCard experienceType='work' />
+                <a href='/Badillo_Mariela_Resume.pdf' target='_blank'>
+                    <button className='bg-charcoal m-8 p-2 text-white rounded-full'>View Full Resume</button>
+                </a>
+            </div>
+
+            {/* education experience */}
+            <div className='basis-1/2'>
+                <div className={`${raleway.className} text-3xl bg-lightBlue w-2/3 p-4 rounded-r-full`}>Education</div>
+                <ExperienceCard experienceType='education' />
+            </div>
+
+            {/* background svg */}
+            <div className='absolute bottom-0 right-0'>
+                <Image
+                    src='/pattern.png'
+                    width={500}
+                    height={500}
+                    className='relative -z-10 scale-x-flip' 
+                    alt='svg image with half circle, star, and pointed flower'
+                />
+            </div>
+        </div>
     )
 }
---
+++ app/lib/experience/projectExperience.ts
@@ -0,0 +1,18 @@
+const ProjectExperience = [
+    {
+        title: 'title',
+        company: 'company',
+        duration: 'duration',
+        description: 'description',
+        skills: 'skills'
+    }
+];
+
+// {
+//     title: 'title',
+//     company: 'company',
+//     duration: 'duration',
+//     description: 'description',
+//     skills: 'skills'
+// }
+export default ProjectExperience; 
