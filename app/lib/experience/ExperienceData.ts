4284f1cb80112e0f16575b12da16ebdeef43bdb1
Mariela Badillo <41022186+MarielaB118@users.noreply.github.com>
2024-06-04 15:41:49 -0400

    Add project data and read me

--- app/lib/experience/ExperienceData.ts
+++ app/lib/experience/ExperienceData.ts
@@ -4,18 +4,50 @@ export const EducationExperience = [
         company: 'Bachelor of Science in Computer Science, minor in Mathematics',
         duration: 'Aug 2020 - May 2024',
         description: 'gpa 3.8 / 4.0',
-        skills: ''
-    }
+        skills: '',
+        link: '',
+    },
 ];
 
 export const ProjectExperience = [
     {
-        title: 'title',
-        company: 'company',
-        duration: 'duration',
-        description: 'description',
-        skills: 'skills'
-    }
+        title: 'Personal Website',
+        company:'',
+        duration: 'May 2024 - Jun 2024',
+        description: `Engineered a sophisticated and responsive personal website utilizing Next.js, TypeScript, and Tailwind CSS to exhibit professional work and achievements.
+                    Learned and utilized cutting-edge web technologies to craft an engaging and visually appealing platform for showcasing skills and portfolio projects to prospective 
+                    employers.`,
+        skills: 'Next.js, TypeScript, Tailwind CSS, design, development, deployment',
+        link: '',
+    },
+    {
+        title: 'CleverCook',
+        company: 'academic project',
+        duration: 'Mar 2024 - May 2024',
+        description: `Employed Express.js and Node.js for efficient backend development, ensuring reliable server-side functionality seamlessly integrated with the React frontend. 
+                    Designed and implemented intuitive user interfaces with React to prioritize user engagement and satisfaction, guiding users seamlessly through recipe discovery, 
+                    preparation, and cooking processes.`,
+        skills: 'React, JavaScript, Express.js, Node.js, user experience, design, front-end development',
+        link: 'https://github.com/ampiaget/HCIProject',
+    },
+    {
+        title: 'Free Quizlet',
+        company: 'academic project',
+        duration: 'Jan 2023 - Apr 2023',
+        description: `Utilized Python Flask for rapid back end API development, seamlessly integrating with MongoDB for scalable data storage and management.
+                    Architected and implemented CRUD operations and RBAC principles, ensuring robust data management and user access control. 
+                    Collaborated with a team to deliver an intuitive user experience, featuring interactive study modes and progress tracking, enhancing learning outcomes.`,
+        skills: 'Python, Python Flask, MongoDB, full-stack development',
+        link: 'https://github.com/Cory-Avrutis/Free-Quizlet',
+    },
+    // {
+    //     title: 'title',
+    //     company: 'company',
+    //     duration: 'duration',
+    //     description: 'description',
+    //     skills: 'skills',
+    //     link: '',
+    // },
 ];
 
 export const WorkExperience = [
@@ -23,21 +55,42 @@ export const WorkExperience = [
         title: 'Research and Development Engineer Intern',
         company: 'Universal Creative - Orlando, FL',
         duration: 'Sep 2023 - Dec 2023',
-        description: 'Applied Python, C++, and JavaScript to develop, debug, and test software components ensuring robustness and functionality across multiple projects.Engineered diverse applications such as facial recognition, facial detection, and identification scanning services, contributing solutions to enhance project functionalities. Assisted in the development and testing phases of prototypes, facilitating the successful deployment of innovative technologies in Universal Destinations and Experiences',
-        skills: 'Python, C++, JavaScript, Software Development, Debugging, Software Testing, Engineering, Prototyping, Adaptability'
+        description: `Applied Python, C++, and JavaScript to develop, debug, and test software components ensuring robustness and functionality across multiple projects.
+                    Engineered diverse applications such as facial recognition, facial detection, and identification scanning services, contributing solutions to enhance 
+                    project functionalities. Assisted in the development and testing phases of prototypes, facilitating the successful deployment of innovative technologies 
+                    in Universal Destinations and Experiences`,
+        skills: 'Python, C++, JavaScript, Software Development, Debugging, Software Testing, Engineering, Prototyping, Adaptability',
+        link: '',
     },
     {
         title: 'Software Development Engineer Intern',
         company: 'Amazon Robotics - Westborough, MA',
         duration: 'May 2023 - Aug 2023',
-        description: 'descri Demonstrated proactive learning by mastering new technologies and tools including C#, Unity, and Git, enhancing personal skill set and adaptability within the development environment. Collaborated with a team of developers to conceptualize, develop, and integrate new features into storage solutions, optimizing user experience and functionality, leading to the successful delivery of an end-to-end functional application meeting project requirements. Engaged in Agile development methodologies by attending daily stand-up meetings and delivering regular status updates on assigned Jira tasks and project advancement, fostering clear communication and alignment with the team.ption',
-        skills: 'C#, Unity, Git, Adaptability, Teamwork, Collaboration, User Experience, Agile, Jira, Communication'
+        description: `Demonstrated proactive learning by mastering new technologies and tools including C#, Unity, and Git, enhancing personal skill set and adaptability within 
+                    the development environment. Collaborated with a team of developers to conceptualize, develop, and integrate new features into storage solutions, optimizing 
+                    user experience and functionality, leading to the successful delivery of an end-to-end functional application meeting project requirements. Engaged in Agile 
+                    development methodologies by attending daily stand-up meetings and delivering regular status updates on assigned Jira tasks and project advancement, fostering 
+                    clear communication and alignment with the team.`,
+        skills: 'C#, Unity, Git, Adaptability, Teamwork, Collaboration, User Experience, Agile, Jira, Communication',
+        link: '',
     },
     {
         title: 'Undergraduate Teaching Assistant',
         company: 'Florida State University',
         duration: 'Aug 2022 -Apr 2023',
-        description: 'Conducted weekly office hours to provide personalized assistance to students, addressing questions and clarifying concepts for the Introduction of C++ course, fostering a supportive learning environment. Delivered constructive and detailed feedback on assignments, providing insights to help students understand their mistakes and make necessary improvements in their work, facilitating academic growth and improvement.',
-        skills: 'C++, Teaching, Communication, Leadership'
+        description: `Conducted weekly office hours to provide personalized assistance to students, addressing questions and clarifying concepts for the Introduction of C++ course, 
+        fostering a supportive learning environment. Delivered constructive and detailed feedback on assignments, providing insights to help students understand their mistakes and 
+        make necessary improvements in their work, facilitating academic growth and improvement.`,
+        skills: 'C++, Teaching, Communication, Leadership',
+        link: '',
     },
 ];
+
+// {
+//     title: 'title',
+//     company: 'company',
+//     duration: 'duration',
+//     description: 'description',
+//     skills: 'skills',
+//     link: '',
+// }
